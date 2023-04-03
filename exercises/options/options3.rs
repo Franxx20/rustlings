c2e32c0580d8e8892b88fc7c6c85ec83a4853f55
Franxx20 <francojg2013.Fg@gmail.com>
2023-04-03 18:25:09 -0300

    Avance de ejercicios
    se llego hasta threads

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,8 +1,6 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -11,9 +9,17 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
+    /*
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
-        _ => println!("no match"),
+    Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+    _ => println!("no match"),
+    }
+    */
+    let a = "Hola!";
+    if let Some(p) = &y {
+        println!("Co-ordeinates are {},{}", p.x, p.y);
+    } else {
+        println!("no match");
     }
     y; // Fix without deleting this line.
 }
